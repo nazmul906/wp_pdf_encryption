b11457ae511362efd4039c74055597c4cdf610ea
nazmul906 <nazmulhb96@gmail.com>
2023-09-10 12:35:46 +0600

    encrpted string is attach in query paramtere when api is hit

---
+++ pdf-viewer-plugin.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Plugin Name: PDF Viewer Plugin
+Version: 1.0
+*/
+
+// Include Composer's autoloader
+require_once(plugin_dir_path(__FILE__) . 'vendor/autoload.php');
+
+// include 'vendor/autoload.php';
+// Initialize the Ncrypt class
+
+$ncrypt = new mukto90\Ncrypt;
+
+
+
+
+
+function register_pdf_viewer_api_endpoint() {
+    register_rest_route('pdf-viewer/v1', '/generate-url', array(
+        'methods' => 'POST',
+        'callback' => function (WP_REST_Request $request) {
+            // Retrieve the encryptionKey and language from the request
+            $encryptionKey = $request->get_param('encryptionKey');
+            $language = $request->get_param('language');
+
+            // Call the generate_pdf_viewer_url function with the extracted parameters
+            $url = generate_pdf_viewer_url($encryptionKey, $language);
+
+            // Return the URL as the response
+            return rest_ensure_response($url);
+        },
+    ));
+}
+
+function generate_pdf_viewer_url($encryptionKey, $language) {
+    global $ncrypt;
+    // update_option('restapi','res');
+    // update_option( 'the_key', $encryptionKey ); 
+    // update_option( 'lan', $language ); 
+    // Replace with your URL generation logic
+
+    $base_url = 'http://localhost/wordpress/page-pdf/';
+    // $base_url = 'http://localhost/wordpress/pdf/';
+    
+
+
+
+
+    // Combine encryptionKey and language into an array and encrypt the result
+    $combined_data = json_encode(array('encryptionKey' => $encryptionKey, 'language' => $language));
+    // update_option( 'combine', $combined_data); 
+    $encrypted_key = $ncrypt->encrypt($combined_data, '34234');
+    // update_option( 'encrypt',  $encrypted_key);
+
+    // Add the encrypted key as a query parameter to the URL
+    // $url = $base_url . '?token=' . urlencode($encrypted_key);
+
+    // Add the encrypted key and language as query parameters to the URL
+    $url = $base_url . '?token=' . urlencode($encrypted_key) . '&language=' . urlencode($language);
+    return $url;
+}
+
+
+
+
+
+
+
+
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fs/pdf-viewer-plugin',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fs/pdf-viewer-plugin' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mukto90/ncrypt' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c66236716492796fcc7c28e07a5e7752daeb9225',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mukto90/ncrypt',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
